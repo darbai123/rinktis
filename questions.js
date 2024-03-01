bd95a2f94c1c4d0114aecd54024a51efca055098
Aurimas <154673326+darbai123@users.noreply.github.com>
2024-03-01 23:40:18 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+
+let questions = [
+    {
+        numb: 1,
+        question: "Geriausias krepšininkas?",
+        answer: "C. Michael Jordan",
+        options: [
+            "A. Arvydas Sabonis",
+            "B. Shaquile O'neil",
+            "C. Michael Jordan",
+            "D. Michael Rodman"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Greičiausias automobilis?",
+        answer: "A. Buggati",
+        options: [
+            "A. Buggati",
+            "B. Audi",
+            "C. BMW",
+            "D. Porsche"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Didžiausias miestas?",
+        answer: "A. Moscow",
+        options: [
+            "A. Moscow",
+            "B. Talin",
+            "C. Vilnius",
+            "D. Ryga"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Stipriausias gyvūnas?",
+        answer: "D. Raganosis",
+        options: [
+            "A. Bizonas",
+            "B. Krokodilas",
+            "C. Liūtas",
+            "D. Raganosis"
+        ]
+    },
+    {
+        numb: 5,
+        question: "daugiausia gyventoju turinti šalis?",
+        answer: "D. Indija",
+        options: [
+            "A. Brazilija",
+            "B. Vokietija",
+            "C. Rusija",
+            "D. Indija"
+        ]
+    },
+];
